cff143d54bf1a018f1e85d5b046132e14fb8577c
Playek-17 <wojciech.arendarski17@gmail.com>
2025-09-15 12:40:59 +0200

    typy proste

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //tu program glowny
+        System.out.println("Dzień dobry");
+
+        //typy proste
+        //pisane z malej litery, zmienne a nie obiekt -> nie mają pól i metod
+        //typy calkowite
+        byte liczba1 = 100;
+        short liczba2 = 300;
+        int liczba3 = 1234567;
+        long liczba4 = 123456789;
+        System.out.println("twoja liczba: "+liczba1);
+
+        //typy zmiennoprzecinkowe, rzeczywiste, z ułamkiem
+        float liczbaRzeczywista1 = 3.14f; //musi byc na koncu f
+        double liczbaRzeczywista2 = 23.456789;
+        System.out.println("Liczba rzeczywista: "+ liczbaRzeczywista2);
+
+        //typ logiczny
+        boolean czyPrawda = true; //moze byc tez false
+
+        //typ znakowy
+        char znak = 'a';
+
+
+
+
     }
 }
